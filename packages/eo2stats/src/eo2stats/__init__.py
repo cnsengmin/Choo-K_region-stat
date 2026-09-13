23ff0fe1085c1ab1c2f04fbc8c0798ad98840d93
cnsengmin <75140439+cnsengmin@users.noreply.github.com>
2026-09-13 23:01:10 +0900

    feat(eo2stats): expose scene search API

--- packages/eo2stats/src/eo2stats/__init__.py
+++ packages/eo2stats/src/eo2stats/__init__.py
@@ -7,13 +7,20 @@ from .models import (
     TransformMethod,
 )
 from .policy import evaluate_spatialization
+from .scenes import CollectionCoverage, SceneRecord, SceneSearchRequest
+from .search import get_dataset_coverage, search_scenes
 
 __all__ = [
+    "CollectionCoverage",
     "EvidenceType",
     "IndicatorSpec",
     "PolicyDecision",
     "QuantityType",
+    "SceneRecord",
+    "SceneSearchRequest",
     "SupportType",
     "TransformMethod",
     "evaluate_spatialization",
+    "get_dataset_coverage",
+    "search_scenes",
 ]
