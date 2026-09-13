dbe223bd7f02de69725e783f649ec9f7392f5765
cnsengmin <75140439+cnsengmin@users.noreply.github.com>
2026-09-14 00:26:10 +0900

    Expose Sentinel-2 index analysis API

--- packages/eo2stats/src/eo2stats/__init__.py
+++ packages/eo2stats/src/eo2stats/__init__.py
@@ -11,21 +11,43 @@ from .models import (
 from .policy import evaluate_spatialization
 from .scenes import CollectionCoverage, SceneRecord, SceneSearchRequest
 from .search import get_dataset_coverage, search_scenes
+from .sentinel2 import (
+    INDEX_SPECS,
+    SCL_CLASS_NAMES,
+    SCL_MASK_POLICIES,
+    IndexSummary,
+    SCLMaskPolicy,
+    SpectralIndexResult,
+    analyze_sentinel2_scene,
+    build_scl_valid_mask,
+    compute_sentinel2_index,
+    get_scl_mask_policy,
+)
 
 __all__ = [
     "AssetSpec",
     "CollectionCoverage",
     "EvidenceType",
+    "INDEX_SPECS",
+    "IndexSummary",
     "IndicatorSpec",
     "PolicyDecision",
     "QuantityType",
     "RasterWindowResult",
+    "SCL_CLASS_NAMES",
+    "SCL_MASK_POLICIES",
+    "SCLMaskPolicy",
     "SceneRecord",
     "SceneSearchRequest",
+    "SpectralIndexResult",
     "SupportType",
     "TransformMethod",
+    "analyze_sentinel2_scene",
+    "build_scl_valid_mask",
+    "compute_sentinel2_index",
     "evaluate_spatialization",
     "get_dataset_coverage",
+    "get_scl_mask_policy",
     "inspect_scene_assets",
     "read_scene_asset_bbox",
     "search_scenes",
